c7035abdd487e653be5cc89eab34ed9efc111ef4
Barbara Mayfield <bmayfield1991@gmail.com>
2019-08-20 21:54:33 -0500

    changed field goal score increment to add 3 instead of 4

--- src/App.js
+++ src/App.js
@@ -42,11 +42,11 @@ function App() {
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button className="homeButtons__touchdown" onClick={() => updateHomeScore(homeScore + 7)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={() => updateHomeScore(homeScore + 4)}>Home Field Goal</button>
+          <button className="homeButtons__fieldGoal" onClick={() => updateHomeScore(homeScore + 3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
           <button className="awayButtons__touchdown" onClick={() => updateAwayScore(awayScore + 7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => updateAwayScore(awayScore + 4)}>Away Field Goal</button>
+          <button className="awayButtons__fieldGoal" onClick={() => updateAwayScore(awayScore + 3)}>Away Field Goal</button>
         </div>
       </section>
     </div>
